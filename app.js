563ad245d173b9ede7f3b4281a7ea580859c9912
afwyou <afwyousenge@gmail.com>
2021-05-01 17:01:38 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
+const exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -13,7 +18,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 
 app.listen(3000, () => {
