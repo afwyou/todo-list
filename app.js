043a73c80a4f8f76c9ef51ef05977a4d34952d96
afwyou <afwyousenge@gmail.com>
2021-05-02 21:55:57 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -43,9 +43,30 @@ app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('detail', { todo }))
+    .then((todo) => res.render('detail', { todo }))//產生了todo這個實例
     .catch(error => console.log(error))
 })
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))//這是fetail頁面的路由
+    .catch(error => console.log(error))
+})
+
 
 
 
