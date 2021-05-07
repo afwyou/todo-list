92788c7ca231634d452c61fd722fc265bc031d4d
afwyou <afwyousenge@gmail.com>
2021-05-08 06:17:33 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const db = mongoose.connection
 const bodyParser = require('body-parser')
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 const methodOverride = require('method-override')
-const routes = require('./routes')
+const routes = require('./routes/index')
+require('./config/mongoose')
 const exphbs = require('express-handlebars');
 
 
@@ -24,6 +25,8 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
+// home.js,todos.js,匯入index.js總路由
+// index.js總路由在匯入app.js
 
 
 app.listen(3000, () => {
