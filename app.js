51b345d51e41ebaddd4f7ed378db0ddf3edebf25
afwyou <afwyousenge@gmail.com>
2021-05-18 05:42:16 +0800

    刪除app.js多餘的多餘的mongoose設定

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
 // const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
